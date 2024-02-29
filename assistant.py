7eea397db15235748fb8159ff2439dfb17b46bd0
Goldenbuilds <goldisaac33@gmail.com>
2024-02-29 13:39:44 +0000

    updates

--- assistant.py
+++ assistant.py
@@ -8,11 +8,26 @@ from PIL import Image, ImageTk
 import speech_recognition as sr
 import requests
 import os
-import speech_recognition as sr 
 import winsound 
 import pyttsx3
 import difflib
+import shutil
 
+# # https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+# def resource_path(relative_path):
+#     """ Get absolute path to resource, works for dev and for PyInstaller """
+#     try:
+#         # PyInstaller creates a temp folder and stores path in _MEIPASS
+#         base_path = sys._MEIPASS2
+#     except Exception:
+#         base_path = os.path.abspath(".")
+
+#     return os.path.join(base_path, relative_path)
+
+# Logo = resource_path("Logo.png")
+
+
+# GUI CODE FOR BESTIE
 class VoiceAssistantGUI:
     def __init__(self):
         self.root = tk.Tk()
@@ -50,7 +65,7 @@ class VoiceAssistantGUI:
         button_frame.pack(pady=5)
 
         # Microphone icon
-        image = Image.open(r"icons/mic.jpg")  #  path to microphone icon
+        image = Image.open(r"icon.ico")  #  path to microphone icon
         image = image.resize((50, 50), Image.LANCZOS)
         microphone_icon = ImageTk.PhotoImage(image)
         self.microphone_label = tk.Label(button_frame, image=microphone_icon, bg=bg_color)
@@ -120,7 +135,9 @@ class VoiceAssistant:
         winsound.Beep(sound_freq, duration)
 
     def activate_assistant(self):
+        self.play_sound(500,600)
         self.gui.update_text("Listening for 'Hey Bestie' ...")
+
         print("Listening for 'Hey Bestie' ...")
 
         r = sr.Recognizer()
@@ -129,27 +146,38 @@ class VoiceAssistant:
         while True:
             with microphone as source:
                 try:
-                    audio = r.listen(source, timeout=5)
-                    detected_phrase = r.recognize_google(audio, language='en-NG').lower()
+                    audio = r.listen(source, timeout=5) # listen to commands
+
+                    detected_phrase = r.recognize_google(audio, language='en-NG').lower() #words extracted from user
+
+                    #feedback mechanisms
                     self.say(f"Detected: {detected_phrase}")
+                    self.gui.update_text(f"Detected: {detected_phrase}")
                     print(f"Detected: {detected_phrase}")
-                    self.gui.update_text(f"{detected_phrase}") #show text in textbox
-                    
-                    if "hey bestie" in detected_phrase:
+
+                    #activation mechanisms for various cases/scenarious
+                    if "hey bestie" in detected_phrase: #user say hey bestie
                         self.say("Hey there! How can i help you? ")
                         self.activate_deletion()
                     
-                    if "go to sleep" in detected_phrase:
+                    if "go to sleep" in detected_phrase: #user wants to end task 
                         self.sleep()
 
-                except sr.WaitTimeoutError:
+                    if "delete a file" in detected_phrase: #user needs to delete a file
+                        self.activate_deletion()
+
+                # Exceptions for programs not to run with feedback to user 
+                except sr.WaitTimeoutError: # for no speech detection
                     print("Timeout. No speech detected.")
+                    self.say("Say something")
                     self.gui.update_text("Timeout. No speech detected")
+
                 except sr.UnknownValueError:
-                    pass  # Ignore if the speech couldn't be recognized
+                    pass  # Ignore if no speech recognized
+
                 except sr.RequestError:
                     print("Failed to request results from Google Speech Recognition service.")
-                    self.gui.update_text("Now listening for command to delete a file.")
+                    self.gui.update_text("Failed to request results from Google Speech Recognition service. Check your internet connection")
     
     def activate_sleepmode(self):
         self.gui.update_text("Sleep mode activated ...")
@@ -165,13 +193,13 @@ class VoiceAssistant:
                     audio = r.listen(source, timeout=5)
                     detected_phrase = r.recognize_google(audio, language='en-NG').lower()
                     print(f"Detected: {detected_phrase}")
+                    self.gui.update_text(f"Detected: {detected_phrase}")
                     self.say(f"Detected: {detected_phrase}")
 
                     if "go to sleep" in detected_phrase or "sleep" in detected_phrase:
                         self.sleep()
-                        break #exit the loop after activating sleep mode
-
-                    else:
+                        break # exit loop 
+                    else: 
                         print("Waiting for  command")
                         self.say("waiting for command")
                 except sr.UnknownValueError:
@@ -179,16 +207,13 @@ class VoiceAssistant:
                 except sr.RequestError:
                     print("Failed to request results from Google Speech Recognition service.")
     
-    
+
     def activate_deletion(self):
-        print("Now listening for command to delete a file.")
         self.gui.update_text("Say 'delete a file for me' ")  
         self.play_sound(500,600)
 
         r = sr.Recognizer()
         microphone = sr.Microphone()
-
-
         while True:
             with microphone as source:
                 try:
@@ -197,66 +222,65 @@ class VoiceAssistant:
                     print(f"Detected: {detected_phrase}")
                     self.gui.update_text(f"Detected: {detected_phrase}")
 
-                    if "delete a file" in detected_phrase or "delete" in detected_phrase or "remove a file" in detected_phrase or "remove" in detected_phrase:
+                    if "delete a file" in detected_phrase or "delete" in detected_phrase:
                         self.say("Okay I'll help you with file deletion")
-                        self.delete()
+                        self.gui.update_text("Okay I'll help with file deletion")
+                        self.delete_file()
                         break #exit the loop after command is processed
                     
                     if "go to sleep" in detected_phrase:
                         self.sleep()
 
                     else:
-                        print("waiting for deletion command")
+                        self.gui.update_text("waiting for deletion command")
                         self.say("You need to tell me to delete a file for you")
                         
                 except sr.RequestError:
-                    print("Failed to request results. Don't worry it is a problem with Google API")
+                    self.gui.update_text("Failed to request results. Don't worry it is a problem with Google API")
                 except sr.UnknownValueError:
                     pass #ignore if the speech couldn't be recognized
 
-    def delete(self):
-        self.say("What file would you like to delete?")
+    def delete_file(self):
         r = sr.Recognizer()
         microphone = sr.Microphone()
-
 
         while True:
             with microphone as source:
                 try:
                     audio = r.listen(source, timeout= 5)
                     detected_phrase = r.recognize_google(audio, language= "en-NG").lower()
-                    print(f"FILENAME DETECTED: {detected_phrase}")
                     self.say("What file would you like to delete?")
                     self.gui.update_text("What file would you like to delete?")
-
-                    #Remove the file extension from the detected phrase
+                    print(f"FILENAME DETECTED: {detected_phrase}")
+                    
+                    #Remove  file extension from  detected phrase
                     detected_phrase_no_extn, _= os.path.splitext(detected_phrase)
 
-                    #Remove spaces and special characters from the detected phrase
+                    #Remove space and special characters from the detected phrase
                     detected_phrase_cleaned = ''.join(char for char in detected_phrase_no_extn if char.isalnum())
-                    
-
-                    #use difflib to find similar filenames
+                
+                    #use difflib to find files with similar names with detected phrase
                     close_matches = difflib.get_close_matches(detected_phrase_cleaned, self.file_system)
-                    # There are close matches  
+
+                    # if there are close matches  
                     if close_matches:
-                        self.say(f"Did you mean {close_matches[0]}?")
-                        users_reply = self.listen().lower()
-                        print(f"User's reply: {users_reply}")
+                        self.say(f"Did you mean {close_matches[0]}?") #ask if user means to delete the close match
+                        users_reply = self.listen().lower() #store user's reply
+                        self.gui.update_text(f" You replied: {users_reply}")
 
                         #confirm if close match is what the user wants
-                        if "yes" in users_reply or "okay" in users_reply:
-                            filename = close_matches[0]
-                            self.confirm_deletion(filename)
+                        if "yes" in users_reply or "okay" in users_reply or "yh " in users_reply:
+                            filename = close_matches[0] #filename is now the match chosen by user
+                            self.confirm_file_deletion(filename) #confirm deletion of the file
                             return filename
 
                     else:
-                        #Exact match is found
+                        # if Exact match is found
                         match = re.search(r'delete\s*(.*)', detected_phrase)
+                        
                         if match:
                             filename = match.group(1).strip() #filename gotten from match
                             print(f"{filename}")
-
 
                             # Remove spaces and special characters from the file
                             filename_cleaned = ''.join(char for char in filename if char.isalnum())
@@ -270,12 +294,12 @@ class VoiceAssistant:
                                 file_no_extn, _ = os.path.splitext(file) #file with no extension
                                 file_no_extn_cleaned = ''.join(char for char in file_no_extn if char.isalnum())
                                 if file_no_extn_cleaned == detected_phrase_no_extn:
-                                    self.confirm_deletion(file)
+                                    self.confirm_file_deletion(file)
                                     filename_found = file
                                     break
 
                             if filename_found:
-                                self.confirm_deletion(filename_found)
+                                self.confirm_file_deletion(filename_found)
                                 return filename_found
 
                     
@@ -299,7 +323,7 @@ class VoiceAssistant:
                 except PermissionError:
                     self.gui.update_text("Permission denied  to delete this file ")
                     print(f"Error: Permission denied to delete file")
-                    self.say("I do not have the permission to delete this file")
+                    self.say("Permission denied to delete this file. Run this program as an Administrator.")
                 
                 except OSError as e:
                     print(f"Error: {e}.")
@@ -307,7 +331,10 @@ class VoiceAssistant:
             return None
             
 
-    def confirm_deletion(self, filename):
+    
+
+
+    def confirm_file_deletion(self, filename):
         while True:
             self.say(f"Are you sure you want to delete {filename}?")
             self.gui.update_text(f"Are you sure you want to delete {filename}?")  # Update text box in GUI
@@ -337,28 +364,18 @@ class VoiceAssistant:
             print("Do you want to delete another file?")
 
             confirm_another = self.listen_for_confirmation()
-            if confirm_another:
-                try:
-                    os.remove(filename)
-                    print(f"{filename} has been deleted successfully")
-                    self.say("Your file has been deleted successfully")
-                    self.gui.update_text(f"{filename} has been deleted successfully")  # Update text box in GUI
-
-                except OSError as e:
-                    print(f"Failed to delete{filename}: {e}")
-                    self.say(f"Failed to delete{filename}: {e}")
-                    self.gui.update_text(f"Failed to delete{filename}: {e}")
-            else:
-                print("Deletion cancelled")
-                self.gui.update_text("Deletion cancelled")  # Update text box in GUI
-                self.say("Deletion cancelled")
-                break # Exit the loop if deletion is cancelled
-            
             if not confirm_another:
-                break #EXit the loop if user does not want another file deleted
-            
-            break  # This break will exit the confirm_deletion function after completing the loop
+                break # exit the loop if user does not want another file deleted
 
+            #Prompt for a new file name 
+            self.say("What file would you like to delete?")
+            self.gui.update_text("What file would you like to delete ")
+            new_filename = self.delete() #call delete again
+            if not new_filename:
+                break #Exit if no new file name provided
+
+        # Exit the confirm_deletion function after completing the loop
+        return
                 
 
     def listen_for_confirmation(self):
@@ -381,12 +398,16 @@ class VoiceAssistant:
                         print("Please respond with yes or no")
                         self.say("Please respond with yes or no")
                         self.gui.update_text("Please respond with a yes or no")
+
                 except sr.RequestError:
                     print("Failed to request results. It seems there is an issue with Google API")
+                    self.gui.update_text("Failed to request results. It seems there is an issue with Google API")
+
                 except sr.UnknownValueError:
                     print("Sorry, I couldn't understand your response. Please try again.")
                     self.say("Sorry, I couldn't understand your response. Please try again.")
                     self.gui.update_text("Sorry, I couldn't understand your response. Please try again.")
+
                 except sr.WaitTimeoutError:
                     print("Timeout. No response detected.")
                     self.say("No response detected. Please try again.")
